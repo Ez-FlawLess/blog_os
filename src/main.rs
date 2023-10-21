02b22da02cca496e90ea6cc1379db6a9a97bd761
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2023-10-21 12:10:15 +0330

    build for our target

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+    loop {
+        
+    }
+}
+
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    loop {}
 }
